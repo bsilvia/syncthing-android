8e5dc445b8fd853655ce935947275adc2cb797d6
bsilvia <5374098+bsilvia@users.noreply.github.com>
2025-11-19 19:32:15 -0800

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:8.13.1")
         classpath("com.github.ben-manes:gradle-versions-plugin:0.36.0")
 
         // NOTE: Do not place your application dependencies here; they belong
@@ -23,5 +23,5 @@ buildscript {
 }
 
 tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
+    delete(getLayout().buildDirectory)
 }
